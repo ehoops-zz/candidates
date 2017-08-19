f5824ea6b29d1e39ea3f59e85a84b0fab7f3b6ef
Erin Hoops <ehoops@users.noreply.github.com>
2017-08-18 20:53:55 -0700

    Overlay shows candidate picture.  Main panel displays user email address along with the name.

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ class App extends Component {
   }
 
   render() {
-    console.log(data);
     let candidateList = data.map((c, ind) => {
       return (
         <Candidate key={`candidate${ind}`}
--- src/Candidate.js
+++ src/Candidate.js
@@ -5,11 +5,15 @@ export default class Candidate extends Component {
 
   render() {
     let info = JSON.parse(this.props.candidateInfo);
+    console.log(info);
     return(
       <div>
         <div className="candidateRow" onClick={() => {
           this.props.onCandidateClick(this.props.id)}}>
-          <div className="name">{info.name}</div>
+          <div className="personal">
+            <div className="name">{info.name}</div>
+            <div className="email">{`user${info.id}@nimble.com`}</div>
+          </div>
           <div className="status">{info.status}</div>
           <div className="applicationDate">{info.applicationDate}</div>
           <div className="action">No Action</div>
@@ -18,7 +22,10 @@ export default class Candidate extends Component {
           <div className="ellipses">...</div>
         </div>
         <div id={`overlay-${this.props.id}`} className="overlay">
-          
+          <img
+            className="picture"
+            src="https://upload.wikimedia.org/wikipedia/commons/c/c1/CGP_Grey_stick_figure.svg"
+            alt="Stick Figure Teacher" />
           <div>Name: {info.name}</div>
           <div>Status: {info.status}</div>
           <div>Application Date: {info.applicationDate}</div>
